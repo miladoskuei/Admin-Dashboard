a215a3b993933286982575da6c8b173fb62eca8a
miladoskuei <miladoskuei@ymail.com>
2024-09-08 11:48:10 +0330

    import fetch func to widgetsm and used it

--- src/components/Topbar/widgetlg/Widgetlg.jsx
+++ src/components/Topbar/widgetlg/Widgetlg.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./widgetls.css";
 import { transactions } from "../../../datas/routes/transaction";
+import { useQueryClient } from "react-query";
 
 export default function Widgetlg() {
   const Button = ({ type }) => (
     <button className={"widgetLgButton  " + type}> {type} </button>
   );
+  const useGetFetchQuery = (name) => {
+    const queryClient = new useQueryClient();
+
+    return queryClient.getQueryData([name]);
+  };
+
+  const data = useGetFetchQuery("Users");
+  console.log("data in useGetFetchQuery is", data);
+
   return (
     <div className="widgetlg">
       <h3 className="widgetLgTitle"> latest transactions </h3>{" "}
@@ -26,7 +36,7 @@ export default function Widgetlg() {
               </span>{" "}
             </td>{" "}
             <td className="widgetLgDate"> {transaction.date} </td>{" "}
-            <td className="widgetLgAmount">{transaction.amount}</td>{" "}
+            <td className="widgetLgAmount"> {transaction.amount} </td>{" "}
             <td className="widgetLgStatus">
               <Button type="Approved"> </Button>{" "}
             </td>{" "}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,21 +6,38 @@ import { costChartDatas } from "../../datas/routes/costChartDatas";
 import Widgetsm from "../../components/Topbar/widgetsm/Widgetsm";
 import Widgetlg from "../../components/Topbar/widgetlg/Widgetlg";
 
-
 export default function Home() {
-
   return (
-    <div style={{flex:4, padding: "1rem",display:'flex', flexDirection:'column', gap:'2rem',overflowX:'hidden' }}>
-      <h1 style={{ textAlign: "center" }}>Welcome dashboard</h1>
-      <Feature></Feature>
-      <Chart yaxis='value' title="salary" grid data={costChartDatas} dataKey="value" ></Chart>
+    <div
+      style={{
+        flex: 4,
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        overflowX: "hidden",
+      }}
+    >
+      <img src="logo192.png"></img>{" "}
+      <h1 style={{ textAlign: "center" }}> Welcome dashboard </h1>{" "}
+      <Feature> </Feature>{" "}
+      <Chart
+        yaxis="value"
+        title="salary"
+        grid
+        data={costChartDatas}
+        dataKey="value"
+      >
+        {" "}
+      </Chart>{" "}
       {/* <p>This is the home page.</p>
-        <Link to={'/users'}>users</Link> */}
-      <div className="widgets" style={{display:'flex',gap:'2rem',flexWrap:'wrap'}}>
-        <Widgetsm></Widgetsm>
-        <Widgetlg></Widgetlg>
-
-      </div>
+                    <Link to={'/users'}>users</Link> */}{" "}
+      <div
+        className="widgets"
+        style={{ display: "flex", gap: "2rem", flexWrap: "wrap" }}
+      >
+        <Widgetsm> </Widgetsm> <Widgetlg> </Widgetlg>
+      </div>{" "}
     </div>
   );
 }
